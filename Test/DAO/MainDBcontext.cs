c5c54b7cd90e7b178c29e6ca0fc82e67dce61cfc
David Muradyan <I22.Muradyan.David@etud.ufar.am>
2024-03-10 00:52:58 +0400

    Add project files.

---
+++ Test/DAO/MainDBcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Entities;
+
+
+namespace Test.DAO {
+    public class MainDBcontext : DbContext {
+        public MainDBcontext(DbContextOptions options) : base(options) {
+        }
+
+        public DbSet<Mard> M { get; set; }
+    }
+}
---
+++ testing/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test.DAO;
+
+namespace testing.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase {
+        MainDBcontext _context;
+
+        public ValuesController(MainDBcontext context) {
+            _context = context;
+        }
+    }
+}
